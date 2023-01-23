08a05e2c7285d19676af597bb152ab1f0d3329fd
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2023-01-23 03:54:26 +0300

    add unit test for parallel versions of RatesCollector

---
+++ src/test/java/valeriy/knyazhev/multithreading/behaviour/ParallelRatesCollectorTest.java
@@ -0,0 +1,69 @@
+package valeriy.knyazhev.multithreading.behaviour;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import valeriy.knyazhev.multithreading.RatesCollector;
+import valeriy.knyazhev.multithreading.model.Rate;
+import valeriy.knyazhev.multithreading.service.RateProvider;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static valeriy.knyazhev.multithreading.model.Rate.rate;
+
+/**
+ * @author Valeriy Knyazhev
+ */
+public class ParallelRatesCollectorTest {
+
+    private static final String SYMBOL = "GBPUSD";
+    private static final Rate RATE_1 = rate()
+        .symbol(SYMBOL)
+        .ask(new BigDecimal("1.215"))
+        .bid(new BigDecimal("1.205"))
+        .build();
+    private static final Rate RATE_2 = rate()
+        .symbol(SYMBOL)
+        .ask(new BigDecimal("1.212"))
+        .bid(new BigDecimal("1.202"))
+        .build();
+
+    private final RateProvider firstProvider = mock(RateProvider.class);
+    private final RateProvider secondProvider = mock(RateProvider.class);
+
+    @BeforeEach
+    public void setup() {
+        when(firstProvider.rateFor(SYMBOL)).thenReturn(RATE_1);
+        when(secondProvider.rateFor(SYMBOL)).thenReturn(RATE_2);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parallelCollectorImplementations")
+    public void should_collect_rates_from_all_providers_sequentially(RatesCollector collector) {
+        // when
+        var result = collector.collectRates(SYMBOL, List.of(firstProvider, secondProvider));
+
+        // then
+        assertThat(result).isNotEmpty();
+        assertThat(result).containsExactlyInAnyOrder(RATE_1, RATE_2);
+
+        // and
+        verify(firstProvider).rateFor(SYMBOL);
+        verify(secondProvider).rateFor(SYMBOL);
+    }
+
+    private static Stream<Arguments> parallelCollectorImplementations() {
+        return Stream.of(
+            Arguments.of(new ThreadsWithJoinRatesCollector()),
+            Arguments.of(new ThreadsWithCountDownLatchRatesCollector())
+        );
+    }
+
+}
